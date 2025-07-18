628e2953f2b8fa34d36389b087e3ef678ec062f7
adminmohammed <105809983+adminmohammed@users.noreply.github.com>
2025-07-18 17:08:46 +0300

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,11 @@ fx_version 'bodacious'
 games {'gta5'}
 lua54 "yes"
 
-escrow_ignore {"config/locations.json","config/addons.json","config/permissions.cfg"}
+escrow_ignore {
+    "config/locations.json",
+    "config/addons.json",
+    "config/permissions.cfg"
+}
 
 name 'vMenu'
 version 'v1.2.2'
@@ -16,13 +20,24 @@ server_debug_mode 'false'
 experimental_features_enabled '0'
 
 
-files {'vMenu/Newtonsoft.Json.dll','vMenu/MenuAPI.dll','config/locations.json','config/addons.json','vMenu/storage.html'}
-server_scripts {'@vrp/lib/utils.lua'}
+files {
+    'vMenu/Newtonsoft.Json.dll',
+    'vMenu/MenuAPI.dll',
+    'config/locations.json',
+    'config/addons.json',
+    'vMenu/storage.html'
+}
+server_scripts {
+    '@vrp/lib/utils.lua'
+}
 
 client_script 'Files/client.lua'
 server_script 'Files/server.lua'
 client_script 'vMenu/vMenuClient.net.dll'
 server_script 'vMenu/vMenuServer.net.dll'
 client_script "@vrp_basic_quest/69.lua"
+
+
+
 
 
